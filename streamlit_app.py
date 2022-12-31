ff53f8b602aaa86f61df2e04858af52db083f817
JithinH <121372024+JithinH@users.noreply.github.com>
2022-12-31 11:52:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,23 @@
 import pandas as pd 
 import streamlit as st
 
-
-# from PIL import Image
-# import pip
-# pip.main(["install", "openpyxl"])
+#from PIL import Image
+#import pip
+#pip.main(["install", "openpyxl"])
 #above three codes can be used if pictures are required
 
 #basic page setup and code for accessing required files
 st.set_page_config(page_title='COMPANY NAME')
 st.header('GLASS CRAFTERS')
 st.subheader('Glass Inventory')
-excel_file=pd.read_excel(r'https://drive.google.com/drive/folders/16XQ1urBhPzrf54ldBMMmPJyJ4_z0NiQB?usp=sharing')
+excel_file=pd.read_excel(r'C:\Users\Jithin H S\Downloads\final_data.xlsx')
 sheet_name='Sheet1'
 
 #data accessing from excel 
-
+df=pd.read_excel(excel_file,
+                sheet_name=sheet_name,
+                usecols='A:E',
+                header=0)
                 
                  
 # st.dataframe(df)
