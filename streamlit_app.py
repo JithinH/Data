55b116ecdb61e262aeeabe08bf108c2a3a92a251
JithinH <121372024+JithinH@users.noreply.github.com>
2023-01-01 10:28:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 #basic page setup and code for accessing required files
 st.set_page_config(page_title='COMPANY NAME')
-st.markdown('<div style="text-align: center;">GLASSCRAFTERS</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;">GLASSCRAFTERS,value60</div>', unsafe_allow_html=True)
 st.markdown('<div style="text-align: center;"Glass Inventory></div>', unsafe_allow_html=True)
 excel_file='final_data_2.xlsx'
 sheet_name='Sheet1'
@@ -25,7 +25,7 @@ df=pd.read_excel(excel_file,
 # st.dataframe(df)
 #search bar
 to_find=st.text_input('Enter glass colour or type',label_visibility="visible")
-makers=df.where((df['COLOUR']==to_find) | (df['TYPE']==to_find) | (df['SPECTRUM CODE']==to_find) | (df['GLASSCRAFTERS CODE']==to_find) | (df['SHADE']==to_find) | (df['NAME']==to_find) | (df['SHELF']==to_find))
+makers=df.where((df['COLOUR']==to_find) | (df['TYPE']==to_find) | (df['SPECTRUM CODE']==to_find) | (df['GLASSCRAFTERS CODE']==to_find) | (df['SHADE']==to_find) | (df['SHELF']==to_find))
 
 
 #display searched elements	
