1b79cfe9e6b63ccb5b4a182edbd446edb4937545
JithinH <121372024+JithinH@users.noreply.github.com>
2023-01-01 11:14:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ df=pd.read_excel(excel_file,
 #search bar
 to_find=st.text_input('Enter glass colour or type',label_visibility="visible")
 makers=(df.where((df['COLOUR']==to_find) | (df['TYPE']==to_find) | (df['SPECTRUM CODE']==to_find) | (df['GLASSCRAFTERS CODE']==to_find) | (df['SHADE']==to_find) | (df['SHELF']==to_find)))
-na=df.style.format(makers=0)
+makers=df.style.format(precision=0)
 
 #display searched elements	
-st.table(na.dropna())
+st.table(makers.dropna())
 
 
